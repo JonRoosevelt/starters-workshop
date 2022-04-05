4915ffc7ead6ddc449cde8f4ba2d3e6dd6209242
Jon Martins <jonathanroosevelt@gmail.com>
2022-04-05 18:21:31 +0100

    added theme-ui with css in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import './App.css';
-import CarShop from './CarShop';
+import { ThemeProvider } from "@emotion/react";
+import CarShop from "./car-shop/CarShop";
+import { theme } from "./theme/theme";
 
 function App() {
-  return <div className="App"><CarShop /></div>
+  return (
+    <ThemeProvider theme={theme}>
+      <CarShop />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/car-shop/CarShop.tsx
@@ -0,0 +1,43 @@
+/** @jsxImportSource theme-ui */
+
+import React, { useState } from "react";
+import { Image, Button, Container, Heading, Box, Card } from "theme-ui";
+
+function CarShop() {
+  const [boughtCar, setBoughtCar] = useState(false);
+
+  const handleBuyCar = (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setBoughtCar(true);
+  };
+
+  return (
+    <Container
+      sx={{
+        maxWidth: 768,
+        color: "primary",
+        fontFamily: "heading",
+      }}
+      className="CarShopPage"
+    >
+      {boughtCar ? (
+        <Card
+          sx={{ position: "absolute", margin: "auto", left: "30%", top: "50%" }}
+        >
+          <Heading>You've bought a car. Congratulations Stevie Wonder</Heading>
+        </Card>
+      ) : (
+        <Box p={3} sx={{ border: "1px solid lightgray", mt: "2vh" }}>
+          <Heading>BMW X1</Heading>
+          <Image src="/bmw-x1.jpeg" alt="BMW X1" />
+          <div>
+            <Button onClick={handleBuyCar}>Buy</Button>
+          </div>
+        </Box>
+      )}
+    </Container>
+  );
+}
+
+export default CarShop;
+
