a8ab8173b5e480bec8999d82626490d16b5f9a46
Jon Martins <jonathanroosevelt@gmail.com>
2022-04-05 16:04:02 +0100

    creates react testing library example

---
+++ src/CarShop.test.tsx
@@ -0,0 +1,38 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import CarShop from "./CarShop";
+
+describe('Buy car', () => {
+    test('should render a car name', () => {
+        render(<CarShop />);
+
+        const carName = screen.getByText('BMW X1')
+        expect(carName).toBeInTheDocument();
+    })
+
+    test('should render an image of a car', () => {
+        render(<CarShop />);
+
+        const carImage = screen.getByAltText('BMW X1');
+        expect(carImage).toBeInTheDocument()
+    })
+
+    test('should render a button to buy the car', () => {
+        render(<CarShop />);
+        const buyButton = screen.getByRole('button', { name: 'Buy' })
+        expect(buyButton).toBeInTheDocument()
+    })
+
+    test('should return bought car message', () => {
+        // Arrange
+        render(<CarShop />)
+
+        // Act
+        const buyButton = screen.getByRole('button', { name: 'Buy' })
+        fireEvent.click(buyButton)
+
+        // Assert
+        const boughtCarMessage = screen.getByText(/You've bought a car. Congratulations Stevie Wonder/i)
+        expect(boughtCarMessage).toBeInTheDocument()
+    })
+})
+
